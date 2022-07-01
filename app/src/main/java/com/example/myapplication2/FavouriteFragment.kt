cf97ca9046f4f981b1389e161ae4d20dc44adbf2
Akklle <sa.ti.le@list.ru>
2022-07-01 16:03:22 +0300

    solved1

--- app/src/main/java/com/example/myapplication2/FavouriteFragment.kt
+++ app/src/main/java/com/example/myapplication2/FavouriteFragment.kt
@@ -4,23 +4,36 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.myapplication2.databinding.FragmentFavouriteBinding
 import com.example.myapplication2.databinding.FragmentMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class FavouriteFragment: Fragment(R.layout.fragment_favourite) {
+    private var _binding: FragmentFavouriteBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentFavouriteBinding.bind(view)
+
 
         val text = arguments?.getString(ARG_TEXT).orEmpty()
 
         if (text.isNotEmpty()) {
             Snackbar.make(view, text, Snackbar.LENGTH_LONG).show()
         }
-        //hhh
+
+        with(binding){
+            btnBack.setOnClickListener(){
+                findNavController().popBackStack(
+                )
+            }
+        }
 
     }
+
 
     companion object {
 
@@ -34,5 +47,9 @@ class FavouriteFragment: Fragment(R.layout.fragment_favourite) {
             )
             return bundle
         }
+    }
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
